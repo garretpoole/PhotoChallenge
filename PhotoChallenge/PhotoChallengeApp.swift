fcd781da9dd5c9fee579402708f5c629311219a8
Garret Poole <garretpoole@users.noreply.github.com>
2022-04-26 15:34:09 -0700

    Initial Commit

---
+++ PhotoChallenge/PhotoChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoChallengeApp.swift
+//  PhotoChallenge
+//
+//  Created by Garret Poole on 4/26/22.
+//
+
+import SwiftUI
+
+@main
+struct PhotoChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
